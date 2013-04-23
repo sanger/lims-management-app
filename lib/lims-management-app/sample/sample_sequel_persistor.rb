eb533abd7cc1077750c269f8c3ef613afa408f9b
Loic <llh1@sanger.ac.uk>
2013-04-23 15:43:30 +0100

    Add a before save filter to generate the sanger sample ID

--- lib/lims-management-app/sample/sample_sequel_persistor.rb
+++ lib/lims-management-app/sample/sample_sequel_persistor.rb
@@ -6,6 +6,25 @@ module Lims::ManagementApp
     class SampleSequelPersistor < SamplePersistor
       include Lims::Core::Persistence::Sequel::Persistor
 
+      def filter_attributes_on_save(attributes)
+        attributes.mash do |k,v|
+          case k
+          when :sanger_sample_id then [:sanger_sample_id, new_sanger_sample_id]
+          else [k,v]
+          end
+        end
+      end
+
+      private
+
+      def new_sanger_sample_id
+        SangerSampleID.generate(unique_identifier)
+      end
+
+      def unique_identifier
+        last_id = dataset.select(:id).limit(1).order(:id).last
+        last_id ? last_id[:id] + 1 : 1
+      end
     end
   end
 end
