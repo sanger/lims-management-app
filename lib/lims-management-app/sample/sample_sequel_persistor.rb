07b1f0c619fe928fb4cec4d6e3a42a9111665362
Loic <llh1@sanger.ac.uk>
2013-04-24 11:24:29 +0100

    Do not set sanger sample id in the persistor

--- lib/lims-management-app/sample/sample_sequel_persistor.rb
+++ lib/lims-management-app/sample/sample_sequel_persistor.rb
@@ -5,24 +5,6 @@ module Lims::ManagementApp
   class Sample
     class SampleSequelPersistor < SamplePersistor
       include Lims::Core::Persistence::Sequel::Persistor
-
-      def filter_attributes_on_save(attributes)
-        attributes.mash do |k,v|
-          case k
-          when :sanger_sample_id then [:sanger_sample_id, new_sanger_sample_id]
-          else [k,v]
-          end
-        end
-      end
-
-      private
-
-      # @return [String]
-      # Generate a new sanger sample id
-      def new_sanger_sample_id
-        SangerSampleID.generate
-      end
     end
   end
 end
-
