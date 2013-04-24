78cbbd6cbfff5ed9ab4982d53163031330d799db
Loic <llh1@sanger.ac.uk>
2013-04-24 15:45:25 +0100

    refactor

--- lib/lims-management-app.rb
+++ lib/lims-management-app.rb
@@ -2,11 +2,11 @@ require 'common'
 
 Lims::Core::NO_AUTOLOAD = true
 
-require 'lims-api/server'
-require 'lims-api/context_service'
-
 require 'lims-management-app/version'
 require 'lims-management-app/sample/all'
+
+require 'lims-api/server'
+require 'lims-api/context_service'
 
 module Lims
   module ManagementApp
