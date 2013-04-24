d8c607b6b6f0be6579def9258e2e8dd051a9ddbd
Loic <llh1@sanger.ac.uk>
2013-04-24 11:24:04 +0100

    Remove initialize, add sanger sample id setter

--- lib/lims-management-app/sample/sample.rb
+++ lib/lims-management-app/sample/sample.rb
@@ -5,6 +5,11 @@ module Lims::ManagementApp
   class Sample
     include Lims::Core::Resource
 
+    # required attributes
+    attribute :sanger_sample_id, String, :required => true, :writer => :private, :initializable => true
+    attribute :gender, String, :required => true, :writer => :private, :initializable => true
+    attribute :sample_type, String, :required => true, :writer => :private, :initializable => true
+
     # The attributes below are all strings, not required with a private writer
     %w(hmdmc_number supplier_sample_name common_name ebi_accession_number sample_source
     mother father sibling gc_content public_name cohort storage_conditions).each do |name|
@@ -17,19 +22,14 @@ module Lims::ManagementApp
     attribute :is_sample_a_control, Boolean, :required => false, :writer => :private, :initializable => true
     attribute :is_re_submitted_sample, Boolean, :required => false, :writer => :private, :initializable => true
 
-    # required attributes
-    attribute :sanger_sample_id, String, :required => true, :writer => :private, :initializable => true
-    attribute :gender, String, :required => true, :writer => :private, :initializable => true
-    attribute :sample_type, String, :required => true, :writer => :private, :initializable => true
-
     validates_with_method :ensure_gender_value
     validates_with_method :ensure_sample_type_value
 
     GENDER = ["Not applicable", "Male", "Female", "Mixed", "Hermaphrodite", "Unkown"]
     SAMPLE_TYPE = ["DNA Human", "DNA Pathogen", "RNA", "Blood", "Saliva", "Tissue Non-Tumour", "Tissue Tumour", "Pathogen"]
 
-    def initialize(*args, &block)
-      super(*args, &block)
+    def generate_sanger_sample_id
+      @sanger_sample_id = SangerSampleID.generate
     end
 
     private
