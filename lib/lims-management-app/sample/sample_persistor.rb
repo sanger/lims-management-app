61c24fe3580e025fb27bb1d568ffaed6d4ff2af2
Loic <llh1@sanger.ac.uk>
2013-04-24 17:17:28 +0100

    Add filter on save and on load to handle DNA datas

--- lib/lims-management-app/sample/sample_persistor.rb
+++ lib/lims-management-app/sample/sample_persistor.rb
@@ -1,11 +1,27 @@
 require 'lims-core/persistence/persistor'
-require 'lims-management-app/sample/sample'
 
 module Lims::ManagementApp
   class Sample
     class SamplePersistor < Lims::Core::Persistence::Persistor
       Model = Sample
+
+      def filter_attributes_on_save(attributes)
+        attributes.mash do |k,v|
+          case k
+          when :dna then [:dna_id, @session.id_for!(v)]
+          else [k,v]
+          end
+        end
+      end
+
+      def filter_attributes_on_load(attributes)
+        attributes.mash do |k,v|
+          case k
+          when :dna_id then [:dna, @session.dna[v]]
+          else [k,v]
+          end
+        end
+      end
     end
   end
 end
-
