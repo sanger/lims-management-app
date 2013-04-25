81b27b4490245cfaf5896c4545980acbdd43196a
Loic <llh1@sanger.ac.uk>
2013-04-25 09:33:45 +0100

    Remove private writer from attributes

--- lib/lims-management-app/sample/sample.rb
+++ lib/lims-management-app/sample/sample.rb
@@ -7,21 +7,21 @@ module Lims::ManagementApp
     include Lims::Core::Resource
 
     # required attributes
-    attribute :sanger_sample_id, String, :required => true, :writer => :private, :initializable => true
-    attribute :gender, String, :required => true, :writer => :private, :initializable => true
-    attribute :sample_type, String, :required => true, :writer => :private, :initializable => true
+    attribute :sanger_sample_id, String, :required => true, :initializable => true
+    attribute :gender, String, :required => true, :initializable => true
+    attribute :sample_type, String, :required => true, :initializable => true
 
     # The attributes below are all strings, not required with a private writer
     %w(hmdmc_number supplier_sample_name common_name ebi_accession_number sample_source
     mother father sibling gc_content public_name cohort storage_conditions).each do |name|
-      attribute :"#{name}", String, :required => false, :writer => :private, :initializable => true
+      attribute :"#{name}", String, :required => false, :initializable => true
     end
 
-    attribute :taxon_id, Numeric, :required => false, :writer => :private, :initializable => true
-    attribute :volume, Integer, :required => false, :writer => :private, :initializable => true
-    attribute :date_of_sample_collection, DateTime, :required => false, :writer => :private, :initializable => true
-    attribute :is_sample_a_control, Boolean, :required => false, :writer => :private, :initializable => true
-    attribute :is_re_submitted_sample, Boolean, :required => false, :writer => :private, :initializable => true
+    attribute :taxon_id, Numeric, :required => false, :initializable => true
+    attribute :volume, Integer, :required => false, :initializable => true
+    attribute :date_of_sample_collection, DateTime, :required => false, :initializable => true
+    attribute :is_sample_a_control, Boolean, :required => false, :initializable => true
+    attribute :is_re_submitted_sample, Boolean, :required => false, :initializable => true
     attribute :dna, Dna, :required => false, :initializable => true
 
     validates_with_method :ensure_gender_value
