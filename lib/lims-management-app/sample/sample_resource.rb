838626cc8bbe308d72f18fb7cafaf404f82afd22
Loic <llh1@sanger.ac.uk>
2013-04-25 13:58:14 +0100

    Override content to stream to display correctly dna and rna datas

--- lib/lims-management-app/sample/sample_resource.rb
+++ lib/lims-management-app/sample/sample_resource.rb
@@ -5,7 +5,25 @@ module Lims::ManagementApp
   class Sample
     class SampleResource < Lims::Api::CoreResource
 
-    end
+      def content_to_stream(s, mime_type)
+        object.attributes.each do |k,v|
+          next if k.to_s == "dna" || k.to_s == "rna"
+          s.add_key k
+          s.add_value v
+        end
+
+        component_to_stream("dna", object.dna, s, mime_type)
+        component_to_stream("rna", object.rna, s, mime_type)
+      end
+
+      private
+
+      def component_to_stream(key, object, s, mime_type)
+        if object
+          s.add_key key
+          hash_to_stream(s, object.attributes, mime_type)
+        end
+      end
+    end 
   end
 end
-
