3dc9cd82c8859ea552e087df9b80e2f4c0604d64
Loic <llh1@sanger.ac.uk>
2013-04-25 11:26:02 +0100

    Not use session#id_for anymore as it only works with Lims::Core::Resource.

--- lib/lims-management-app/sample/sample_persistor.rb
+++ lib/lims-management-app/sample/sample_persistor.rb
@@ -8,10 +8,24 @@ module Lims::ManagementApp
       def filter_attributes_on_save(attributes)
         attributes.mash do |k,v|
           case k
-          when :dna then [:dna_id, @session.id_for!(v)]
+          when :dna then [:dna_id, save_component(v)]
           else [k,v]
           end
         end
+      end
+
+      # @param [Object] object
+      # @return [Integer]
+      # In that case, object is not a Lims::Core::Resource object.
+      # Indeed, sample contains DNA/RNA/... data but there are not
+      # resources. They are only part of a sample. As a result, we
+      # cannot use the @session.id_for!(object) as it only works 
+      # with Lims::Core::Resource. Here we try to get an id for that
+      # object, if we cannot find it in session, we save the object
+      # and get its id.
+      def save_component(object)
+        return nil unless object
+        @session.persistor_for(object).id_for(object) || @session.save(object)
       end
 
       def filter_attributes_on_load(attributes)
