3dbce5774e3b9c53148d5190dcb3c0ef5ba0164a
Loic <llh1@sanger.ac.uk>
2013-04-30 09:43:58 +0100

    requirements

--- lib/lims-management-app/sample/all.rb
+++ lib/lims-management-app/sample/all.rb
@@ -1,11 +1,14 @@
 require 'lims-management-app/sample/dna/all'
 require 'lims-management-app/sample/rna/all'
 require 'lims-management-app/sample/cellular_material/all'
+
 require 'lims-management-app/sample/sample'
 require 'lims-management-app/sample/sample_persistor'
 require 'lims-management-app/sample/sample_sequel_persistor'
 require 'lims-management-app/sample/sample_resource'
+
 require 'lims-management-app/sample/create_sample'
-require 'lims-management-app/sample/create_bulk_sample'
-require 'lims-management-app/sample/create_bulk_sample_resource'
+require 'lims-management-app/sample/bulk_create_sample'
+require 'lims-management-app/sample/bulk_create_sample_resource'
 require 'lims-management-app/sample/update_sample'
+require 'lims-management-app/sample/bulk_update_sample'
