abdace4b9153cc2146fad6461b6d71a56dc07e13
Loic <llh1@sanger.ac.uk>
2013-04-30 15:55:13 +0100

    Override delete_raw to delete dna, rna and cellular_material records after deleting a sample

--- lib/lims-management-app/sample/sample_sequel_persistor.rb
+++ lib/lims-management-app/sample/sample_sequel_persistor.rb
@@ -9,6 +9,21 @@ module Lims::ManagementApp
       def self.table_name
         :samples
       end
+
+      private
+
+      def delete_raw(object, id, *params)
+        sample_id = super
+        components = [object.dna, object.rna, object.cellular_material]
+        components.each do |component|
+          if component
+            persistor = @session.persistor_for(component)
+            persistor_dataset = persistor.dataset
+            persistor_dataset.filter(persistor.primary_key => persistor.id_for(component)).delete
+          end
+        end
+        sample_id
+      end
     end
   end
 end
