75d156caeec0cafeefbf3b4d9bed158ef67f542c
Loic <llh1@sanger.ac.uk>
2013-04-30 09:45:30 +0100

    Add table name

--- lib/lims-management-app/sample/sample_sequel_persistor.rb
+++ lib/lims-management-app/sample/sample_sequel_persistor.rb
@@ -1,10 +1,14 @@
-require 'lims-core/persistence/sequel/persistor'
 require 'lims-management-app/sample/sample_persistor'
+require 'lims-core/persistence/sequel/persistor'
 
 module Lims::ManagementApp
   class Sample
-    class SampleSequelPersistor < SamplePersistor
+    class SampleSequelPersistor < Sample::SamplePersistor
       include Lims::Core::Persistence::Sequel::Persistor
+
+      def self.table_name
+        :samples
+      end
     end
   end
 end
