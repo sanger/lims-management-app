f71d6aecf9ba7d4a89b3f840cdb41f6a949cec65
Loic <llh1@sanger.ac.uk>
2013-04-30 10:32:58 +0100

    Specs passing for bulk update by sanger sample id

---
+++ spec/integrations/requests/apiary/11_sample_resource/7_bulk_update_samples_by_sanger_sample_id_spec.rb
@@ -0,0 +1,176 @@
+require "integrations/requests/apiary/11_sample_resource/spec_helper"
+describe "bulk_update_samples_by_sanger_sample_id", :sample => true do
+  include_context "use core context service"
+  before do
+  module Lims::ManagementApp::Sample::SangerSampleID
+    def self.generate
+      @count ||= 5
+      @count += 1
+      "S2-test" << @count.to_s << "-ID"
+    end
+  end
+  end
+  it "bulk_update_samples_by_sanger_sample_id" do
+    sample = Lims::ManagementApp::Sample.new({
+        "gender" => "Male",
+        "sample_type" => "RNA",
+        "taxon_id" => 1,
+        "date_of_sample_collection" => "2013-04-25 10:27 UTC",
+        "is_sample_a_control" => true,
+        "is_re_submitted_sample" => false,
+        "hmdmc_number" => "number",
+        "supplier_sample_name" => "name",
+        "common_name" => "name",
+        "ebi_accession_number" => "number",
+        "sample_source" => "source",
+        "mother" => "mother",
+        "father" => "father",
+        "sibling" => "sibling",
+        "gc_content" => "content",
+        "public_name" => "name",
+        "cohort" => "cohort",
+        "storage_conditions" => "conditions"
+    })
+    sample.generate_sanger_sample_id
+    
+    sample2 = Lims::ManagementApp::Sample.new({
+        "gender" => "Male",
+        "sample_type" => "RNA",
+        "taxon_id" => 1,
+        "date_of_sample_collection" => "2013-04-25 10:27 UTC",
+        "is_sample_a_control" => true,
+        "is_re_submitted_sample" => false,
+        "hmdmc_number" => "number",
+        "supplier_sample_name" => "name",
+        "common_name" => "name",
+        "ebi_accession_number" => "number",
+        "sample_source" => "source",
+        "mother" => "mother",
+        "father" => "father",
+        "sibling" => "sibling",
+        "gc_content" => "content",
+        "public_name" => "name",
+        "cohort" => "cohort",
+        "storage_conditions" => "conditions"
+    })
+    sample2.generate_sanger_sample_id
+    
+    save_with_uuid sample => [1,2,3,4,5], sample2 => [1,2,3,4,6]
+
+    header('Accept', 'application/json')
+    header('Content-Type', 'application/json')
+
+    response = post "/actions/bulk_update_sample", <<-EOD
+    {
+    "bulk_update_sample": {
+        "sanger_sample_ids": [
+            "S2-test6-ID",
+            "S2-test7-ID"
+        ],
+        "volume": 5000
+    }
+}
+    EOD
+    response.status.should == 200
+    response.body.should match_json <<-EOD
+    {
+    "bulk_update_sample": {
+        "actions": {
+        },
+        "user": "user",
+        "application": "application",
+        "result": {
+            "samples": [
+                {
+                    "actions": {
+                        "read": "http://example.org/11111111-2222-3333-4444-555555555555",
+                        "create": "http://example.org/11111111-2222-3333-4444-555555555555",
+                        "update": "http://example.org/11111111-2222-3333-4444-555555555555",
+                        "delete": "http://example.org/11111111-2222-3333-4444-555555555555"
+                    },
+                    "uuid": "11111111-2222-3333-4444-555555555555",
+                    "sanger_sample_id": "S2-test6-ID",
+                    "gender": "Male",
+                    "sample_type": "RNA",
+                    "hmdmc_number": "number",
+                    "supplier_sample_name": "name",
+                    "common_name": "name",
+                    "ebi_accession_number": "number",
+                    "sample_source": "source",
+                    "mother": "mother",
+                    "father": "father",
+                    "sibling": "sibling",
+                    "gc_content": "content",
+                    "public_name": "name",
+                    "cohort": "cohort",
+                    "storage_conditions": "conditions",
+                    "taxon_id": 1,
+                    "volume": 5000,
+                    "date_of_sample_collection": "2013-04-25T11:27:00+01:00",
+                    "is_sample_a_control": true,
+                    "is_re_submitted_sample": false
+                },
+                {
+                    "actions": {
+                        "read": "http://example.org/11111111-2222-3333-4444-666666666666",
+                        "create": "http://example.org/11111111-2222-3333-4444-666666666666",
+                        "update": "http://example.org/11111111-2222-3333-4444-666666666666",
+                        "delete": "http://example.org/11111111-2222-3333-4444-666666666666"
+                    },
+                    "uuid": "11111111-2222-3333-4444-666666666666",
+                    "sanger_sample_id": "S2-test7-ID",
+                    "gender": "Male",
+                    "sample_type": "RNA",
+                    "hmdmc_number": "number",
+                    "supplier_sample_name": "name",
+                    "common_name": "name",
+                    "ebi_accession_number": "number",
+                    "sample_source": "source",
+                    "mother": "mother",
+                    "father": "father",
+                    "sibling": "sibling",
+                    "gc_content": "content",
+                    "public_name": "name",
+                    "cohort": "cohort",
+                    "storage_conditions": "conditions",
+                    "taxon_id": 1,
+                    "volume": 5000,
+                    "date_of_sample_collection": "2013-04-25T11:27:00+01:00",
+                    "is_sample_a_control": true,
+                    "is_re_submitted_sample": false
+                }
+            ]
+        },
+        "taxon_id": null,
+        "volume": 5000,
+        "date_of_sample_collection": null,
+        "is_sample_a_control": null,
+        "is_re_submitted_sample": null,
+        "hmdmc_number": null,
+        "supplier_sample_name": null,
+        "common_name": null,
+        "ebi_accession_number": null,
+        "sample_source": null,
+        "mother": null,
+        "father": null,
+        "sibling": null,
+        "gc_content": null,
+        "public_name": null,
+        "cohort": null,
+        "storage_conditions": null,
+        "dna": null,
+        "rna": null,
+        "cellular_material": null,
+        "sample_uuids": null,
+        "sanger_sample_ids": [
+            "S2-test6-ID",
+            "S2-test7-ID"
+        ],
+        "gender": null,
+        "sample_type": null
+    }
+}
+    EOD
+
+  end
+end
