a1e94c294e783a4c617b33a7a990a634858c3944
Loic <llh1@sanger.ac.uk>
2013-05-02 15:35:07 +0100

    add comments

--- lib/lims-management-app/sample/component.rb
+++ lib/lims-management-app/sample/component.rb
@@ -1,5 +1,8 @@
 module Lims::ManagementApp
   class Sample
+    # The component module is used in Dna/Rna/CellularMaterial classes
+    # as we don't want to make them behave like a Lims::Core::Resource.
+    # In fact, they are only part of a sample.
     module Component
       def initialize(*args, &block)
         options = args.extract_options!.rekey { |k| k.to_sym }
