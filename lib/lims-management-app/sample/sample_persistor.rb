7ef363707133c6704fc0942ad164bd4a4f769c9c
Loic <llh1@sanger.ac.uk>
2013-05-15 14:42:17 +0100

    Handle taxonomy data on save and on load

--- lib/lims-management-app/sample/sample_persistor.rb
+++ lib/lims-management-app/sample/sample_persistor.rb
@@ -6,15 +6,28 @@ module Lims::ManagementApp
       Model = Sample
 
       def filter_attributes_on_save(attributes)
-        attributes.mash do |k,v|
+        taxon_id = attributes[:taxon_id]
+        attributes.reject { |k,v| k == :taxon_id }.mash do |k,v|
           case k
           when :dna then [:dna_id, save_component(v)]
           when :rna then [:rna_id, save_component(v)]
           when :cellular_material then [:cellular_material_id, save_component(v)]
           when :genotyping then [:genotyping_id, save_component(v)]
+          when :scientific_name then [:scientific_taxon_id, taxonomy_primary_id(taxon_id, v, "scientific")]
+          when :common_name then [:common_taxon_id, taxonomy_primary_id(taxon_id, v, "common")]
           else [k,v]
           end
         end
+      end
+
+      # @param [Integer] taxon_id
+      # @param [String] name
+      # @param [String] type
+      # @return [Integer,Nil]
+      # Return the taxonomy id based on the taxon id, 
+      # the name and type in parameters.
+      def taxonomy_primary_id(taxon_id, name, type)
+        @session.persistor_for(:taxonomy).id_by_taxon_id_and_name(taxon_id, name, type)
       end
 
       # @param [Object] object
@@ -38,8 +51,13 @@ module Lims::ManagementApp
           when :rna_id then [:rna, @session.rna[v]]
           when :cellular_material_id then [:cellular_material, @session.cellular_material[v]]
           when :genotyping_id then [:genotyping, @session.genotyping[v]]
+          when :scientific_taxon_id then [:scientific_name, @session.taxonomy[v].name]
+          when :common_taxon_id then [:common_name, @session.taxonomy[v].name]
           else [k,v]
           end
+        end.tap do |a|
+          id = attributes[:scientific_taxon_id]
+          a[:taxon_id] = @session.taxonomy[id].taxon_id 
         end
       end
     end
