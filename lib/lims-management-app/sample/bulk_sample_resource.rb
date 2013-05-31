ece503058f851a41bdf2534a7f5cd36735b6600a
Loic <llh1@sanger.ac.uk>
2013-05-31 10:58:39 +0100

    Remove local object_to_stream. Bulk actions json correctly handled in lims-api

--- lib/lims-management-app/sample/bulk_sample_resource.rb
+++ lib/lims-management-app/sample/bulk_sample_resource.rb
@@ -4,36 +4,6 @@ require 'lims-api/struct_stream'
 module Lims::ManagementApp
   class Sample
     module BulkSampleResource
-      # Override object to stream to display correctly 
-      # the samples in the JSON.
-      # Remove the keys "sample" in front of the sample
-      # objects.
-      def object_to_stream(object, s, mime_type, in_hash = true)
-        case object
-        when Hash
-          s.start_hash unless in_hash
-          object.each  do |k, v|
-            s.add_key k
-            object_to_stream(v, s, mime_type, false)
-          end
-          s.end_hash unless in_hash
-        when Lims::Core::Resource
-          encoder = @context.encoder_for(object, [mime_type])
-          s.with_hash do
-            encoder.to_hash_stream(s)
-          end
-        when Array
-          s.with_array do
-            object.each do |v|
-              object_to_stream(v, s, mime_type, false)
-            end
-          end
-        else
-          s.add_value object
-        end
-      end
-      private :object_to_stream
-
       module Encoder
         include Lims::Api::Resource::Encoder
 
