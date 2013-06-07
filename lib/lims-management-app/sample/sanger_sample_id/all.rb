3948c2df92e14bd8178378bcd0d5e8c867edda6f
Loic <llh1@sanger.ac.uk>
2013-06-07 14:39:49 +0100

    Add persistence and sequel class to access sanger_sample_id_number

---
+++ lib/lims-management-app/sample/sanger_sample_id/all.rb
@@ -0,0 +1,2 @@
+require 'lims-management-app/sample/sanger_sample_id/sanger_sample_id_number_persistor'
+require 'lims-management-app/sample/sanger_sample_id/sanger_sample_id_number_sequel_persistor'
