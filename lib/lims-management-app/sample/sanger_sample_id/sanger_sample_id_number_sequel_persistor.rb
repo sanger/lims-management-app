54133b2f7e2337fb5294dc3008617e908aa60dda
Loic <llh1@sanger.ac.uk>
2013-09-24 13:55:36 +0100

    Lock sanger_sample_id_number table for multithreading environment

--- lib/lims-management-app/sample/sanger_sample_id/sanger_sample_id_number_sequel_persistor.rb
+++ lib/lims-management-app/sample/sanger_sample_id/sanger_sample_id_number_sequel_persistor.rb
@@ -12,9 +12,30 @@ module Lims::ManagementApp
         end
 
         def generate_new_number
-          new_number = self.dataset.where(:id => 1).first[:number] + 1
-          self.dataset.where(:id => 1).update(:number => new_number)
-          new_number
+          lock(dataset, true) do |d|
+            new_number = d.where(:id => 1).first[:number] + 1
+            d.where(:id => 1).update(:number => new_number)
+            new_number
+          end
+        end
+
+        private
+
+        # TODO: when using lims-core version 3, remove these methods and use
+        # lock method from persistence/session.rb.
+        def lock(datasets, unlock=false, &block)
+          datasets = [datasets] unless datasets.is_a?(Array)
+          db = datasets.first.db
+          return lock_for_update(datasets, &block) if db.adapter_scheme =~ /sqlite/i
+
+          db.run("LOCK TABLES #{datasets.map { |d| "#{d.first_source} WRITE"}.join(",")}")
+          block.call(*datasets).tap { db.run("UNLOCK TABLES") if unlock }
+        end
+
+        def lock_for_update(datasets, &block)
+          datasets.first.db.transaction do
+            block.call(*datasets.map(&:for_update))
+          end
         end
       end
     end
