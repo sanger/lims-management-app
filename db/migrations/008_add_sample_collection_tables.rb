52c8c01c88d85e4a490a412b9809d25586b98a1b
Loic <llh1@sanger.ac.uk>
2013-09-25 16:39:30 +0100

    Migration for sample collection tables

---
+++ db/migrations/008_add_sample_collection_tables.rb
@@ -0,0 +1,50 @@
+Sequel.migration do
+  change do
+    create_table :collections do
+      primary_key :id
+      String :type, :null => false
+      index :type
+    end
+
+    create_table :collection_data_string do
+      primary_key :id
+      foreign_key :collection_id, :collections, :key => :id
+      String :name
+      String :value
+    end
+
+    create_table :collection_data_bool do
+      primary_key :id
+      foreign_key :collection_id, :collections, :key => :id
+      String :name
+      Bool :value
+    end
+
+    create_table :collection_data_int do
+      primary_key :id
+      foreign_key :collection_id, :collections, :key => :id
+      String :name
+      Integer :value
+    end
+
+    create_table :collection_data_url do
+      primary_key :id
+      foreign_key :collection_id, :collections, :key => :id
+      String :name
+      String :value
+    end
+
+    create_table :collection_data_uuid do
+      primary_key :id
+      foreign_key :collection_id, :collections, :key => :id
+      String :name
+      String :value, :fixed => true, :size => 64
+    end
+
+    create_table :collections_samples do
+      primary_key :id
+      foreign_key :collection_id, :collections, :key => :id
+      foreign_key :sample_id, :samples, :key => :id
+    end
+  end
+end
