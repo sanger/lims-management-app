63525e74b5827fe3edccb918a9c0176779c76e51
Loic <llh1@sanger.ac.uk>
2013-10-01 09:20:32 +0100

    Display the collections of the sample

--- lib/lims-management-app/sample/sample_resource.rb
+++ lib/lims-management-app/sample/sample_resource.rb
@@ -7,7 +7,7 @@ module Lims::ManagementApp
 
       def content_to_stream(s, mime_type)
         object.attributes.each do |k,v|
-          next if ["dna", "rna", "cellular_material", "genotyping"].include?(k.to_s)
+          next if ["dna", "rna", "cellular_material", "genotyping", "sample_collections"].include?(k.to_s)
           s.add_key k
           s.add_value v
         end
@@ -16,6 +16,8 @@ module Lims::ManagementApp
         component_to_stream("rna", object.rna, s, mime_type)
         component_to_stream("cellular_material", object.cellular_material, s, mime_type)
         component_to_stream("genotyping", object.genotyping, s, mime_type)
+
+        sample_collections_to_stream(object.sample_collections, s, mime_type)
       end
 
       private
@@ -24,6 +26,27 @@ module Lims::ManagementApp
         if object
           s.add_key key
           hash_to_stream(s, object.attributes, mime_type)
+        end
+      end
+
+      def sample_collections_to_stream(sample_collections, s, mime_type)
+        unless sample_collections.nil? || sample_collections.empty?
+          s.add_key "sample_collections"
+          s.with_array do
+            sample_collections.each do |collection|
+              sample_collection_to_stream(collection, s, mime_type)
+            end
+          end
+        end
+      end
+
+      def sample_collection_to_stream(sample_collection, s, mime_type)
+        encoder = @context.encoder_for(sample_collection, [mime_type])
+        s.with_hash do
+          encoder.actions_to_stream(s)
+          s.add_key :uuid
+          s.add_value encoder.object.uuid
+          encoder.object.content_to_stream(s, mime_type, true)
         end
       end
     end 
