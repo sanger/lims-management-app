b738dda135c7d7b269277fa01a3a563a429c5e0a
Loic <llh1@sanger.ac.uk>
2013-10-03 15:06:15 +0100

    Move discovery_type_of in ActionShared and use it in prepared_data

--- lib/lims-management-app/sample-collection/action_shared.rb
+++ lib/lims-management-app/sample-collection/action_shared.rb
@@ -7,10 +7,22 @@ module Lims::ManagementApp
       # Map the data hash to SampleCollectionData objects
       def prepared_data
         data.map do |d|
-          SampleCollectionData.const_get(d["type"].capitalize).new({
-            :key => d["key"],
-            :value => d["value"]
-          })
+          key, value = d["key"], d["value"]
+          type = d["type"] || self.class.discover_type_of(value)
+          SampleCollectionData.const_get(type.capitalize).new(:key => key, :value => value)
+        end
+      end
+
+      # @param [String] value
+      # @return [String]
+      def self.discover_type_of(value)
+        case value
+        when Integer then SampleCollectionData::DATA_TYPE_INT 
+        when TrueClass then SampleCollectionData::DATA_TYPE_BOOL
+        when FalseClass then SampleCollectionData::DATA_TYPE_BOOL 
+        when ValidationShared::VALID_URL_PATTERN then SampleCollectionData::DATA_TYPE_URL 
+        when ValidationShared::VALID_UUID_PATTERN then SampleCollectionData::DATA_TYPE_UUID 
+        else SampleCollectionData::DATA_TYPE_STRING
         end
       end
 
