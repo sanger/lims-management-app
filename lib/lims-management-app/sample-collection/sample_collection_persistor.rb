397b8fc90406b4d54e4fb2121a202bebfa3a13fe
Loic <llh1@sanger.ac.uk>
2013-10-16 12:12:13 +0100

    in progress, sample work

--- lib/lims-management-app/sample-collection/sample_collection_persistor.rb
+++ lib/lims-management-app/sample-collection/sample_collection_persistor.rb
@@ -1,89 +1,119 @@
 require 'lims-core/persistence/persistor'
-require 'lims-management-app/sample-collection/data/data_types_persistor'
+require 'lims-core/persistence/persist_association_trait'
+#require 'lims-management-app/sample-collection/data/data_types_persistor'
+require 'lims-laboratory-app/container_persistor_trait'
 
 module Lims::ManagementApp
   class SampleCollection
-    class SampleCollectionPersistor < Lims::Core::Persistence::Persistor
-      Model = SampleCollection
+    (does "lims/core/persistence/persistable", :children => [
+      {:name => :collection_sample, :session_name => :collection_sample, :deletable => true}
+      #{:name => :collection_data_string, :deletable => true}
+    ]).class_eval do
 
-      # @param [Integer] collection_id
-      # @param [SampleCollection] collection
-      def save_children(collection_id, collection)
-        collection.samples.each do |sample|
-          collection_sample.save_as_association(collection_id, sample) 
-        end
-
-        collection.data.each do |data|
-          @session.save(data, collection_id)
+      def children_collection_sample(sample_collection, children)
+        sample_collection.samples.each do |sample|
+          children << SampleCollectionPersistor::CollectionSample.new(sample_collection, sample)
         end
       end
 
-      # @param [Integer] collection_id
-      # @param [SampleCollection] collection
-      # About @in_sample:
-      # If the collection is loaded in a context a client asks for a sample 
-      # resource (the collection is part of the sample resource), we do not
-      # load the collection samples.
-      def load_children(collection_id, collection)
-        unless @in_sample
-          collection_sample.load_samples(collection_id) do |sample|
-            collection.samples << sample
+#      def children_data_string(collection, children)
+#        collection.data.select { |d| d.class::TYPE == "string" }.each do |data|
+#          children << data
+#        end
+#      end
+
+
+      association_class "CollectionSample" do
+        attribute :sample_collection, SampleCollection, :relation => :parent, :skip_parents_for_attributes => true
+        attribute :sample, Sample, :relation => :parent
+
+        def on_load
+          @sample_collection.samples << @sample if @sample_collection && @sample
+        end
+
+        def invalid?
+          @sample && !@sample_collection.samples.include?(@sample)
+        end
+      end
+
+      class self::CollectionSample
+        class CollectionSampleSequelPersistor < self::CollectionSamplePersistor
+          include Lims::Core::Persistence::Sequel::Persistor
+          def self.table_name
+            :collections_samples
           end
-        end
-
-        load_data(collection_id) do |data|
-          collection.data << data
-        end
-      end
-
-      def in_sample!
-        @in_sample = true
-      end
-
-      def reset_in_sample
-        @in_sample = false
-      end
-
-      # @param [Hash] attributes
-      # @return [Hash]
-      def filter_attributes_on_save(attributes)
-        attributes.delete(:samples)
-        attributes.delete(:data)
-        attributes
-      end
-
-      # @return [CollectionSamplePersistor]
-      def collection_sample
-        @session.collection_sample
-      end
-
-      # @param [Integer] collection_id
-      # @param [Block] block
-      def load_data(collection_id, &block)
-        SampleCollectionData::DATA_TYPES.map do |type|
-          @session.send("collection_data_#{type}")
-        end.each do |data_persistor|
-          data_persistor.load_data(collection_id).each do |data|
-            block.call(data_persistor.get_or_create_single_model(data[:id], data))
-          end
-        end
-      end
-
-      SampleCollectionData::DATA_TYPES.each do |type|
-        define_method("collection_data_#{type}") do
-          @session.send("collection_data_#{type}")
         end
       end
     end
 
 
-    class CollectionSample
-      SESSION_NAME = :collection_sample
-      class CollectionSamplePersistor < Lims::Core::Persistence::Persistor
-        def load_samples(collection_id, &block)
-          raise NotImplementedError, "load_samples is not implemented"
-        end
+    class SampleCollectionSequelPersistor < SampleCollectionPersistor
+      include Lims::Core::Persistence::Sequel::Persistor
+      def self.table_name
+        :collections
       end
     end
   end
 end
+
+  ###################
+  # Collection data #
+  ###################
+  #
+  #    does "lims/laboratory_app/container_persistor", :element => :data_string_proxy, 
+  #      :table_name => :collection_data_string, :contained_class => SampleCollectionData::String
+  #
+  #    class SampleCollectionPersistor
+  #      def children_data_string_proxy(collection, children)
+  #        collection.data.select { |d| d.class::TYPE == "string" }.each do |data|
+  #          data_string_proxy = DataStringProxy.new(collection, nil, data)
+  #          state = @session.state_for(data_string_proxy)
+  #          state.resource = data_string_proxy
+  #          children << data_string_proxy
+  #        end
+  #      end
+  #    
+  #      class DataStringProxy
+  #        def attributes
+  #          @string ? @string.attributes.merge(:sample_collection => @sample_collection) : {}
+  #        end
+  #
+  #        def invalid?
+  #          !@sample_collection.data.include?(@string)
+  #        end
+  #
+  #        def on_load
+  #          @sample_collection.data << @string
+  #        end
+  #
+  #        class DataStringProxyPersistor
+  #          def attribute_for(key)
+  #            {sample_collection: 'sample_collection_id'}[key]
+  #          end
+  #
+  #          def self.table_name
+  #            :collection_data_string
+  #          end
+  #
+  #          def new_from_attributes(attributes)
+  #            collection = @session.sample_collection[attributes.delete(:sample_collection_id)]
+  #            super(attributes) do |attr|
+  #              data_string = SampleCollectionData::String.new(attr)
+  #              model.new(collection, nil, data_string).tap do |proxy|
+  #                proxy.on_load
+  #              end
+  #            end
+  #          end
+  #
+  #          def parents(resource)
+  #            super(resource)
+  #          end
+  #
+  #          def parents_for_attributes(attr)
+  #            []
+  #          end
+  #        end
+  #      end
+  #    end
+
+
