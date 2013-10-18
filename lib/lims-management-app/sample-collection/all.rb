16ab6df269a256d5460089c3fdb5a223d3bc817a
Loic <llh1@sanger.ac.uk>
2013-10-18 14:58:49 +0100

    Remove sequel persistor

--- lib/lims-management-app/sample-collection/all.rb
+++ lib/lims-management-app/sample-collection/all.rb
@@ -1,7 +1,6 @@
 require 'lims-management-app/sample-collection/data/all'
 require 'lims-management-app/sample-collection/sample_collection'
 require 'lims-management-app/sample-collection/sample_collection_persistor'
-require 'lims-management-app/sample-collection/sample_collection_sequel_persistor'
 require 'lims-management-app/sample-collection/sample_collection_resource'
 
 require 'lims-management-app/sample-collection/create_sample_collection'
