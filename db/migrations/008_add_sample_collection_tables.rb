45e7c911ea314662bf586e7e09500a919346efbf
Loic <llh1@sanger.ac.uk>
2013-10-21 16:56:28 +0100

    Rename collection_id to sample_collection_id

--- db/migrations/008_add_sample_collection_tables.rb
+++ db/migrations/008_add_sample_collection_tables.rb
@@ -8,49 +8,49 @@ Sequel.migration do
 
     create_table :collection_data_string do
       primary_key :id
-      foreign_key :collection_id, :collections, :key => :id
+      foreign_key :sample_collection_id, :collections, :key => :id
       String :key
       String :value
-      index [:key, :collection_id], :unique => true
+      index [:key, :sample_collection_id], :unique => true
     end
 
     create_table :collection_data_bool do
       primary_key :id
-      foreign_key :collection_id, :collections, :key => :id
+      foreign_key :sample_collection_id, :collections, :key => :id
       String :key
       TrueClass :value
-      index [:key, :collection_id], :unique => true
+      index [:key, :sample_collection_id], :unique => true
     end
 
     create_table :collection_data_int do
       primary_key :id
-      foreign_key :collection_id, :collections, :key => :id
+      foreign_key :sample_collection_id, :collections, :key => :id
       String :key
       Integer :value
-      index [:key, :collection_id], :unique => true
+      index [:key, :sample_collection_id], :unique => true
     end
 
     create_table :collection_data_url do
       primary_key :id
-      foreign_key :collection_id, :collections, :key => :id
+      foreign_key :sample_collection_id, :collections, :key => :id
       String :key
       String :value
-      index [:key, :collection_id], :unique => true
+      index [:key, :sample_collection_id], :unique => true
     end
 
     create_table :collection_data_uuid do
       primary_key :id
-      foreign_key :collection_id, :collections, :key => :id
+      foreign_key :sample_collection_id, :collections, :key => :id
       String :key
       String :value, :fixed => true, :size => 64
-      index [:key, :collection_id], :unique => true
+      index [:key, :sample_collection_id], :unique => true
     end
 
     create_table :collections_samples do
       primary_key :id
-      foreign_key :collection_id, :collections, :key => :id
+      foreign_key :sample_collection_id, :collections, :key => :id
       foreign_key :sample_id, :samples, :key => :id
-      index [:collection_id, :sample_id], :unique => true
+      index [:sample_collection_id, :sample_id], :unique => true
     end
   end
 end
