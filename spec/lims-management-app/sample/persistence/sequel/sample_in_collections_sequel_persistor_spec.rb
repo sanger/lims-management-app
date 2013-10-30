c915edba471f2519f3b4e5e99fee77490280d172
Loic <llh1@sanger.ac.uk>
2013-10-30 17:06:00 +0000

    Testing deletion of a sample should delete the row in collections_samples table. Not passing

--- spec/lims-management-app/sample/persistence/sequel/sample_in_collections_sequel_persistor_spec.rb
+++ spec/lims-management-app/sample/persistence/sequel/sample_in_collections_sequel_persistor_spec.rb
@@ -10,16 +10,25 @@ module Lims::ManagementApp
 
     let(:sample_collection_uuid) { "11111111-2222-0000-2222-111111111111" }
     let!(:sample_collection) { new_sequel_sample_collection(sample_collection_uuid, sample_collection_action_sample_uuids) }  
+    let(:sample_uuid) { "11111111-0000-0000-0000-222222222222" }  
 
-    context "sample is reloadable with the sample collections it belongs to" do
-      let(:sample_uuid) { "11111111-0000-0000-0000-222222222222" }  
-
-      it "reloads the sample with the sample collections" do
+    context "when loading the sample" do
+      it "reloads the sample with its sample collections" do
         store.with_session do |session|
           sample = session[sample_uuid]
           sample.sample_collections.size.should == 1 
           session.uuid_for(sample.sample_collections[0]).should == sample_collection_uuid
         end
+      end
+    end
+
+    context "when deleting the sample" do
+      it "deletes the rows in collections_samples table" do
+        expect do
+          store.with_session do |session|
+            session.delete(session[sample_uuid])
+          end
+        end.to change { db[:collections_samples].count }.by(-1)
       end
     end
   end
