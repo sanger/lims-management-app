2f7015688522c0a42391035037761ae2adf163f2
Loic <llh1@sanger.ac.uk>
2013-10-31 12:37:22 +0000

    Add comments and do not display sample collections part of sample when we access a sample collection

--- lib/lims-management-app/sample-collection/sample_collection_resource.rb
+++ lib/lims-management-app/sample-collection/sample_collection_resource.rb
@@ -5,7 +5,7 @@ module Lims::ManagementApp
   class SampleCollection
     class SampleCollectionResource < Lims::Api::CoreResource
 
-      # @param [StructStream] s
+      # @param [Stream] s
       # @param [String] mime_type
       # @param [Bool] without_samples
       def content_to_stream(s, mime_type, without_samples = false)
@@ -16,31 +16,38 @@ module Lims::ManagementApp
 
         s.add_key "data"
         s.with_hash do
-          data_to_stream(object.data, s)
+          object.data.each do |d|
+            s.add_key d.key
+            s.add_value d.value
+          end
         end
 
+        # Samples are displayed only if a sample collection is accessed.
+        # In fact, in case we load a sample, we do not want to load:
+        # sample -> sample collections -> samples but just 
+        # sample -> sample collections
         unless without_samples
           s.add_key "samples"
           s.with_array do
-            samples_to_stream(object.samples, s, mime_type)
+            samples_to_stream(object.samples, s, mime_type, true)
           end
         end
       end
 
       private
 
-      def data_to_stream(data, s)
-        data.each do |d|
-          s.add_key d.key
-          s.add_value d.value
-        end
-      end
-
-      def samples_to_stream(samples, s, mime_type)
+      # @param [Hash] samples
+      # @param [Stream] s
+      # @param [String] mime_type
+      # @param [Bool] without_sample_collections
+      def samples_to_stream(samples, s, mime_type, without_sample_collections)
         samples.each do |sample|
           encoder = @context.encoder_for(sample, [mime_type])
           s.with_hash do
-            encoder.to_hash_stream(s)
+            encoder.actions_to_stream(s)
+            s.add_key :uuid
+            s.add_value encoder.object.uuid
+            encoder.object.content_to_stream(s, mime_type, without_sample_collections)
           end
         end
       end
--- lib/lims-management-app/sample/sample_resource.rb
+++ lib/lims-management-app/sample/sample_resource.rb
@@ -5,7 +5,10 @@ module Lims::ManagementApp
   class Sample
     class SampleResource < Lims::Api::CoreResource
 
-      def content_to_stream(s, mime_type)
+      # @param [Stream] s
+      # @param [String] mime_type
+      # @param [Bool] without_sample_collections
+      def content_to_stream(s, mime_type, without_sample_collections = false)
         object.attributes.each do |k,v|
           next if ["dna", "rna", "cellular_material", "genotyping", "sample_collections"].include?(k.to_s)
           s.add_key k
@@ -17,11 +20,21 @@ module Lims::ManagementApp
         component_to_stream("cellular_material", object.cellular_material, s, mime_type)
         component_to_stream("genotyping", object.genotyping, s, mime_type)
 
-        sample_collections_to_stream(object.sample_collections, s, mime_type)
+        # Sample collections are displayed only if a sample is accessed.
+        # In fact, in case a sample collection is loaded, we do not want to load:
+        # sample collection -> samples -> sample collections but just
+        # sample collection -> samples
+        unless without_sample_collections
+          sample_collections_to_stream(object.sample_collections, s, mime_type)
+        end
       end
 
       private
 
+      # @param [String] key
+      # @param [Object] object
+      # @param [Stream] s
+      # @param [String] mime_type
       def component_to_stream(key, object, s, mime_type)
         if object
           s.add_key key
@@ -29,24 +42,23 @@ module Lims::ManagementApp
         end
       end
 
+      # @param [Hash] sample_collections
+      # @param [Stream] s
+      # @param [String] mime_type
       def sample_collections_to_stream(sample_collections, s, mime_type)
         unless sample_collections.nil? || sample_collections.empty?
           s.add_key "sample_collections"
           s.with_array do
             sample_collections.each do |collection|
-              sample_collection_to_stream(collection, s, mime_type)
+              encoder = @context.encoder_for(collection, [mime_type])
+              s.with_hash do
+                encoder.actions_to_stream(s)
+                s.add_key :uuid
+                s.add_value encoder.object.uuid
+                encoder.object.content_to_stream(s, mime_type, true)
+              end
             end
           end
-        end
-      end
-
-      def sample_collection_to_stream(sample_collection, s, mime_type)
-        encoder = @context.encoder_for(sample_collection, [mime_type])
-        s.with_hash do
-          encoder.actions_to_stream(s)
-          s.add_key :uuid
-          s.add_value encoder.object.uuid
-          encoder.object.content_to_stream(s, mime_type, true)
         end
       end
     end 
